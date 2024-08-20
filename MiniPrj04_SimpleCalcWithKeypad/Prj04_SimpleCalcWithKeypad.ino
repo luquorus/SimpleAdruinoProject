6ba4ee49f5bc600b54154d17ee03f9e625ac3f96
Nguyen Lan Nhi <162776742+luquorus@users.noreply.github.com>
2024-08-20 16:35:59 +0700

    Update Prj04_SimpleCalcWithKeypad.ino

--- MiniPrj04_SimpleCalcWithKeypad/Prj04_SimpleCalcWithKeypad.ino
+++ MiniPrj04_SimpleCalcWithKeypad/Prj04_SimpleCalcWithKeypad.ino
@@ -1,7 +1,9 @@
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
 
+// SDA - A4, SCL - A5
 // Initialize the LCD object with I2C address 0x27, 16 columns, 2 rows
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
